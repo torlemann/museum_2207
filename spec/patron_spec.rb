e3298719025493a8d83b7ff375443d66541250e8
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-07-27 08:23:56 -0700

    Add readable attributes test for patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Exhibit do
   it 'exists' do
     expect(patron).to be_a(Patron)
   end
+
+  it 'has readable attributes' do
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+    expect(patron_1.interests).to eq([])
+  end
 end
